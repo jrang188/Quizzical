926654010f2aa1cf3557c421eae6fb916825f3c5
Justin Ang <jrang@sfu.ca>
2023-04-21 16:21:46 -0700

    init new branch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "airbnb",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
   },
-  plugins: ["react"],
-  rules: {},
-};
+}
