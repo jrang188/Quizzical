50172ad211cb420529ff5bd9f6c3b9b208215113
Justin Ang <jrang@sfu.ca>
2023-04-22 22:23:34 -0700

    data fetching from opentriviadb using axios done

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from 'react';
 import Question from './Question';
-import data from '../data.json';
+import axios from 'axios';
+// import data from '../data.json';
+
+interface DataProps {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
 
 export default function Quiz() {
-  const questions = data.results.map((q) => {
+  const [data, setData] = useState<DataProps[]>([]);
+
+  useEffect(() => {
+    const client = axios.create({
+      baseURL: 'https://opentdb.com/api.php',
+    });
+
+    const getQuestions = async () => {
+      const res = await client.get('?amount=5');
+      setData(res.data.results);
+    }
+
+    getQuestions();
+  }, []);
+
+  const questions = data.map((q) => {
+    console.log(data);
     const answers = [q.correct_answer, ...q.incorrect_answers];
     return <Question question={q.question} answers={answers} />;
   });
-
-  // console.log(data.results[0].question);
 
   return (
     <div className="w-5/6">
