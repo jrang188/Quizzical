e97a8c43527710c87ea62cf25dbc1b7f05e1295e
Justin Ang <jrang@sfu.ca>
2023-04-24 22:51:12 -0700

    only shuffle if not t/f questions

--- src/functions/shuffleArray.ts
+++ src/functions/shuffleArray.ts
@@ -1,4 +1,8 @@
 const shuffleArray = (array: string[]) => {
+  if (array.length === 2) {
+    return ['True', 'False'];
+  }
+
   const shuffledArray = [...array];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
