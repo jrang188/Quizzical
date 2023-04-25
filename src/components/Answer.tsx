b62354715aa02d298fcb513f0393cad0b5c4aa56
Justin Ang <jrang@sfu.ca>
2023-04-24 19:55:11 -0700

    added conditional styling for buttons when selected

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,14 +1,19 @@
 import parse from 'html-react-parser';
+import { useState } from 'react';
 
 interface AnswerProps {
   answer: string;
 }
 
 const Answer = ({ answer }: AnswerProps) => {
+  const [selected, setSelected] = useState(false);
+
   return (
     <button
       type="button"
-      className="border-[#4D5B9E] border-solid border rounded-md min-w-[66px] px-1.5"
+      className={`rounded-md min-w-[66px] px-1.5 ${
+        selected ? 'bg-[#D6DBF5]' : 'border-[#4D5B9E] border-solid border'
+      }`}
     >
       <div className="font-inter font-medium text-sm text-[#293264]">
         {parse(answer)}
