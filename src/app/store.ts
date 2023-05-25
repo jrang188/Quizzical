b2e78fadf320a88baf78da67cab034b49ceb1b3a
Justin Ang <jrang@sfu.ca>
2023-05-24 21:00:28 -0700

    added reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { quizOptionsSlice } from "./optionSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    quizOptions: quizOptionsSlice.reducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
